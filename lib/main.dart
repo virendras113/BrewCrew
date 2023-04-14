29c562302e20cb30e0b870899a00277f2f2e53c3
Virendra Shinde <91678077+virendras113@users.noreply.github.com>
2023-04-14 15:58:41 +0530

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:crew_brew/models/user.dart';
+import 'package:crew_brew/screens/wrapper.dart';
+import 'package:crew_brew/services/auth.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return StreamProvider<User>.value(
+      value: AuthService().user,
+      child: MaterialApp(
+        home: Wrapper(),
+      ),
+    );
+  }
+}
+
